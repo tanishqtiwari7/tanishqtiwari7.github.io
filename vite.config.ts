01e79ba7f0b4e8443667c10edb8ea3d9209962d7
Tanishq Tiwari <tanishqtiwari2020@gmail.com>
2026-05-18 23:30:41 +0530

    fix: update Vite configuration and package.json for gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/tanishqtiwari7.github.io/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
